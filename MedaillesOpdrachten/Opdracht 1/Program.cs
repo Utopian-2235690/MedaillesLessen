930e7e79620d40ab167429f1772ee255dd2ee083
Utopian-2235690 <2235690@talnet.nl>
2025-11-12 10:10:27 +0100

    Opdracht 1 gemaakt en betere structuur gemaakt voor de opdrachten.
    Ik heb opdracht 1 afgemaakt en een betere structuur gemaakt om de opdrachten beter te kunnen indelen.

---
+++ MedaillesOpdrachten/Opdracht 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedaillesOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Voer jouw naam in:");
+            string naam = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Voer jouw leeftijd in:");
+            string leeftijd = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Hoi " + naam + ", je bent " + leeftijd + " jaar oud!");
+        }
+    }
+}
